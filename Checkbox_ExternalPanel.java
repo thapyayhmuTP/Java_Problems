902d35779006f165c1e08a429d563eeaf7df3a5d
Tha Pyay Hmu <104383728+thapyayhmuTP@users.noreply.github.com>
2023-08-05 22:19:54 +0100

    Update Checkbox_ExternalPanel.java

--- Checkbox_ExternalPanel.java
+++ Checkbox_ExternalPanel.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+External class for Checkbox-demo
  */
 package Java_Problems;
 import java.awt.*;
